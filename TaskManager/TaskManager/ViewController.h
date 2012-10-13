fe96953aa9c6e373e1d547e97ee900f0a81b1896
Erik Witt <0witt@informatik.uni-hamburg.de>
2012-10-13 12:08:07 +0200

    Universal Projekt TaskManager erstellt.

---
+++ TaskManager/TaskManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TaskManager
+//
+//  Created by Erik Witt on 13.10.12.
+//  Copyright (c) 2012 Erik Witt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TaskManager/TaskManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TaskManager
+//
+//  Created by Erik Witt on 13.10.12.
+//  Copyright (c) 2012 Erik Witt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
